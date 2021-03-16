75dcfeaebdd40473683d3712a3edc00135e28476
DanielMolinaR <dani99mr31@gmail.com>
2021-03-16 13:23:04 +0100

    configuration file changed

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  devServer: {
-    host: "testdomain",
-    port: 8081,
-    https: false,
+  chainWebpack: (config) => {
+    config.performance.maxEntrypointSize(400000).maxAssetSize(400000);
+  },
+  pwa: {
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js",
+    },
+    themeColor: "#fffafa",
+    msTileColor: "#fffafa",
+    appleMobileWebAppCache: "yes",
+    manifestOptions: {
+      background_color: "#fffafa",
+    },
   },
 };
